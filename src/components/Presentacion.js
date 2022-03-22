f75720789ffb662e70e67385fb1f9fa56061e4ea
brenda <lamasbrenda@hotmail.com>
2022-03-22 12:18:43 -0300

    arregle rutas

--- src/components/Presentacion.js
+++ src/components/Presentacion.js
@@ -4,6 +4,7 @@ import Card from "@mui/material/Card";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
 const Presentacion = ({imagen, titulo, link}) => {
@@ -16,22 +17,29 @@ const Presentacion = ({imagen, titulo, link}) => {
             m:0.5, 
             bgcolor:"transparent"
         }}>
-            <ImageList sx={{ width: 222, height: 400, m:0}} cols={1} rowHeight={350}>
-                <ImageListItem>
-                    <img
-                        src= {imagen}
-                        srcSet= {imagen}
-                        alt= {titulo}
-                        loading="lazy"
-                    />
-                </ImageListItem>
-            </ImageList>
-            <Typography variant="subtitle1" color="#FF7E88" 
-            fontWeight="bold" textAlign="center">
-                {titulo}
-            </Typography>
             <Link to={`/carddetallepelicula/${link}`}>
-                <RemoveRedEyeIcon sx={{color:"#FF7E88", m:1.5}}/>
+                <ImageList sx={{ width: 222, height: 400, m:0}} cols={1} rowHeight={350}>
+                    <ImageListItem>
+                        <img
+                            src= {imagen}
+                            srcSet= {imagen}
+                            alt= {titulo}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                </ImageList>
+                <Typography 
+                    variant="subtitle1" 
+                    color="#FF7E88" 
+                    fontWeight="bold" 
+                    textAlign="center">
+                    {titulo}
+                </Typography>
+                <Box sx={{display:"flex", justifyContent:"center"}}>
+                    <RemoveRedEyeIcon sx={{color:"#FF7E88", m:1.5}}/>
+                </Box>
+
+                
             </Link>
 
         </Card>
