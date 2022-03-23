247563ad21bab2693aed08ec651acdda515de1f5
brenda <lamasbrenda@hotmail.com>
2022-03-22 22:11:52 -0300

    buscar elevo estado

--- src/hook/useFetchPeliculas.js
+++ src/hook/useFetchPeliculas.js
@@ -4,7 +4,7 @@ const useFetchPeliculas = (categoria) =>{
     const [pelicula, setPelicula] = useState([])
 
     useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/movie/${categoria}?api_key=6e372c30d27676867cdbcfd7e00f4cf2&language=es-AR&page=1`)
+        fetch(`https://api.themoviedb.org/3/movie/${categoria}?api_key=6e372c30d27676867cdbcfd7e00f4cf2&language=es-ES&page=1`)
         .then(res=>res.json())
         .then(data=>{
             setPelicula(data.results)
