81fc5656cfbf947c2752a43ced1e7d26894e5d21
brenda <lamasbrenda@hotmail.com>
2022-04-06 20:31:23 -0300

    falla lenguaje busqueda

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,7 +1,7 @@
 export const urlBase = 'https://api.themoviedb.org/3/';
 export const apiKey = 'api_key=6e372c30d27676867cdbcfd7e00f4cf2';
 export const queryParamPagina = '&page=';
-
+export const searchMulti = "/search/multi"
 export const cortarArrayPeliculas = array => array.slice(0, 8);
 
 export const definirURL = (categoria, tipo) => `${tipo}/${categoria}`;
@@ -9,3 +9,5 @@ export const definirURL = (categoria, tipo) => `${tipo}/${categoria}`;
 export const definirURLDetalles = (categoria, id) => `${categoria}/${id}`;
 
 export const queryParamLenguaje =(language) => `&language=${language}`;
+
+export const queryParamSearch =(busqueda) => `&query=${busqueda}`;
